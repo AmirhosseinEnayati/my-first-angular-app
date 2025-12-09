7b32054c3de34831b8b02bbfcafaef364c2d14bb
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-10 00:47:48 +0330

    Add user component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./header/header";
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Header]
+  imports: [Header, User]
 })
 export class App {
   protected readonly title = signal('my-first-angular-app');
