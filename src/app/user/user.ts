06f437f79b419949f635760dca9759a0bed3f35b
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-12 10:40:16 +0330

    Use classic state management

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+const users = [
+  { name: 'amirhossein', avatar: 'user_amirhossein.jpg' },
+  { name: 'jennifer', avatar: 'user_jennifer.jpg' },
+];
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +12,14 @@ import { Component } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
+  selectedUser = users[0];
 
+  get imagePath(): string {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    let randomIndex = Math.floor(Math.random() * users.length);
+    this.selectedUser = users[randomIndex];
+  }
 }
