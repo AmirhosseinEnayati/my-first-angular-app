2be2fd5eb3fffec5e876316177bea286449915ad
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-19 00:26:39 +0330

    Add module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { Header } from './header/header';
-import { User } from './user/user';
-import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Header, User, Tasks],
+  standalone: false
 })
 export class App {
   protected readonly title = signal('my-first-angular-app');
