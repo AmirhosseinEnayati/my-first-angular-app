e6b5c6d3b8a671b2cad7cbcfe51858dbce41a5f2
Amirhossein Enayati <enayati.amirh@gmail.com>
2025-12-19 11:17:21 +0330

    Add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { App } from './app';
 import { Header } from './header/header';
 import { Tasks } from './tasks/tasks';
 import { User } from './user/user';
-import { Card } from './shared/card/card';
 import { Task } from './tasks/task/task';
 import { NewTask } from './tasks/new-task/new-task';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/card/card.module';
 
 @NgModule({
-  declarations: [App, Header, Card, User, Tasks, Task, NewTask],
+  declarations: [App, Header, User, Tasks, Task, NewTask],
   bootstrap: [App],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
